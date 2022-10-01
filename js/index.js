4bd527a1d01ea293c189436d09548b58014c0452
aniestkh <108396590+aniestkh@users.noreply.github.com>
2022-10-01 13:46:16 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -3,20 +3,23 @@ $('.menu div .menulist').on('click', function(){
   let artlist = $('article').eq(lino)
   let par = $(this).parent() // = div
 
+  // article에 on이 있을시 이를 제거하고 .menu div에 on 적용(헤더 내 메뉴들 각 다른 줄 적용 ) 
   if($(artlist).hasClass('on')){
     $(artlist).removeClass('on')
     $(this).parent().removeClass('on') 
   } else {
-    $(artlist).addClass('on').siblings().removeClass('on')
-    $(this).parent().addClass('on').siblings().removeClass('on')
+    $(artlist).addClass('on').siblings().removeClass('on')  //선택 article의 이동 및 그 외 article 숨김 
+    $(this).parent().addClass('on').siblings().removeClass('on')  //선택 article 내 요소 출력 
   }
   
+  // 각 article의 제목 내용 이동
 if($(par).hasClass('on')){
   $(artlist).find('.tit, .sub, .tex').addClass('on')
   $(artlist).siblings().find('.tit, .sub, .tex').removeClass('on')
 }
 })
 
+// 화면 로드시 헤더와 헤더 내 메뉴 이동
 $(window).on('load', function(){
   $('#header').css({left:'-6%'})
   $('.menulist').addClass('on')
